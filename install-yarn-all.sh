b07bcb83c9dac1c73c73bcaa1a3aaa25de6eb227
HellCatVN <hellcatvn@gmail.com>
2021-05-01 00:01:06 +0700

    New Script

---
+++ install-yarn-all.sh
@@ -0,0 +1,7 @@
+cd hoimetruyen-api && yarn
+cd ../hoimetruyen-site && yarn
+cd ../hoimetruyen-admin && yarn
+cd ../hoimetruyen-manager && yarn
+cd ../hoimetruyen-evil && yarn
+cd ../hoimetruyen-models && yarn
+cd ../hoimetruyen-modules && yarn
